cadeffa08d21d36f5370d30848c88224f6780c00
Mohamed Ahmed <md.tell414@gmail.com>
2026-05-02 21:56:40 +0200

    final version

--- server.js
+++ server.js
@@ -39,10 +39,13 @@ app.get("/bookings", async (req, res) => {
 });
 
 app.delete("/delete/:id", async (req, res) => {
-  await Booking.findByIdAndDelete(req.params.id);
+  await 
+  Booking.findByIdAndDelete(req.params.id);
   res.send("Deleted");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
